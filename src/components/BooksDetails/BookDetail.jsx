c920c430e855e4833dae573ccb826297103f5d4a
AmirHossain58 <ahantar58@gmail.com>
2024-03-27 05:19:24 +0600

    Listed Books section  added

--- src/components/BooksDetails/BookDetail.jsx
+++ src/components/BooksDetails/BookDetail.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { useLoaderData, useParams } from 'react-router-dom';
+import { Link, useLoaderData, useParams } from 'react-router-dom';
 
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import useBooksData from './../Hooks/useBooksData';
 import {
     Card,
@@ -12,66 +15,107 @@ import {
 import Deatails from '../Deatails';
 
 const BookDetail = () => {
+  const [read ,setRead]=useState(true)
   const [book,setBook]=useState()
   const{books ,loading}=useBooksData()
+  const allBook=useLoaderData()
+  console.log(allBook);
     const{id}=useParams()
-    // const {bookId,bookName,author,image,review,totalPages,rating,category,tags,publisher,yearOfPublishing}=book
+   
     useEffect(()=>{
       if(books){
-        const book2=books.filter(book=>id.includes(book.bookId))
+        const book2=allBook.filter(book=>id.includes(book.bookId))
         setBook(book2);
         // console.log(book2);
       }
       console.log(book);
        
     },[id,books])
-    
+    const {bookId,bookName,author,image,review,totalPages,rating,category,tags,publisher,yearOfPublishing}=book||{}
+    const handleRead=()=>{
+
+
+      if(read){
+
+        toast("Book Added to Read List");
+        setRead(false)
+      }else{
+        toast.error("You Have Already Read this Book");
+      }
+    }
+    const handleWishlist=()=>{
+      if(read){
+
+        toast("Book Added to WishlistList ");
+      }else{
+        toast.error("You Have Already Read this Book");
+      }
+    }
     return (
-      <Card className="w-full max-w-[48rem] flex-row">
+      <Card className="w-full flex-row mt-14">
       <CardHeader
         shadow={false}
         floated={false}
-        className="m-0 w-2/5 shrink-0 rounded-r-none"
+        className="m-0 w-3/6 bg-[#1313130D] p-16 shrink-0 mr-12 rounded-r-none"
       >
         <img
-          // src={image}
+          src={image}
           alt="card-image"
           className="h-full w-full object-cover"
         />
       </CardHeader>
       <CardBody>
-        <Typography variant="h6" color="gray" className="mb-4 uppercase">
-          startups
+        <Typography variant="h4" color="black" className="mb-4 uppercase ">
+        The Catcher in the Rye
         </Typography>
-        <Typography variant="h4" color="blue-gray" className="mb-2">
-          Lyft launching cross-platform service this week
+        <Typography variant="h6" color="blue-gray" className="mb-2">
+        By : Awlad Hossain
         </Typography>
-        <Typography color="gray" className="mb-8 font-normal">
-          Like so many organizations these days, Autodesk is a company in
-          transition. It was until recently a traditional boxed software company
-          selling licenses. Yet its own business model disruption is only part
-          of the story
+        <p>
+        Fiction
+        </p>
+        <Typography color="gray" className="mb-8 font-normal">Sit amet consectetur. Interdum porta pulvinar non sit aliquam. Aenean pulvinar blandit vel non enim elementum penatibus pellentesque ac. Nec accumsan euismod nulla adipiscing lectus. Morbi elementum a auctor erat diam tellus. Fermentum faucibus nulla enim ornare.
+        Id neque neque pretium enim platea urna non dictum. Faucibus dignissim ridiculus nibh tristique massa non.
+        <div className='flex justify-between font-bold'>
+        Tag
+        <p className='text-green-700'>#Young Adult</p>
+        <p className='text-green-700'>#Young Adult</p>
+        </div>
+        <div className="divider"></div>
+        <div className='space-y-2'>
+        <div className=' flex gap-16 '>
+          <p className='text-[#131313B3]'>Number of Pages: </p>
+          <p className='font-bold text-black'>281</p>
+          </div>
+         <div className=' flex gap-16 '>
+          <p className='text-[#131313B3]'>Number of Pages: </p>
+          <p className='font-bold text-black'>281</p>
+          </div>  
+        <div className=' flex gap-16 '>
+          <p className='text-[#131313B3]'>Number of Pages: </p>
+          <p className='font-bold text-black'>281</p>
+          </div>
+        <div className=' flex gap-16 '>
+          <p className='text-[#131313B3]'>Number of Pages: </p>
+          <p className='font-bold text-black'>281</p>
+          </div>
+        </div>
+       
         </Typography>
-        <a href="#" className="inline-block">
-          <Button variant="text" className="flex items-center gap-2">
-            Learn More
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-              className="h-4 w-4"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-              />
-            </svg>
+  
+          <div className='flex gap-2'>
+          <Link to={''}>
+          <Button onClick={handleRead} variant="text" >
+          Read
           </Button>
-        </a>
+          </Link>
+          <Link to={''}>
+          <Button onClick={handleWishlist} color="blue"> Wishlist</Button>
+          </Link>
+          
+          </div>
       </CardBody>
+      <ToastContainer />
     </Card>
     );
 };
--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -1,6 +1,7 @@
 import { Button, Typography } from '@material-tailwind/react';
 import React, { useState } from 'react';
 import { GoChevronDown } from "react-icons/go";
+import { Link } from 'react-router-dom';
 
 const ListedBooks = () => {
     const[click ,setclick]=useState(false)
@@ -13,12 +14,22 @@ const ListedBooks = () => {
         <div className='flex flex-col items-center justify-center  rounded-xl'>
        <div> <Button color='green' className='flex gap-3' onClick={()=>setclick(!click)}>Sort By<GoChevronDown className='text-xl' /></Button></div>
         <div className={click?'inline':'hidden'}>
-            <ul className={'bg-[#1313130D] p-2 rounded-2xl'}>
+            <ul className={'bg-[#1313130D] p-2 rounded-2xl text-center text-[#131313CC]'}>
             <li>Rating</li>
             <li>Number of pages</li>
             <li>Publisher year</li>
         </ul></div>
         </div>
+
+        <div role="tablist" className="tabs tabs-lifted">
+            <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Read Books" />
+            <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">Tab content 1</div>
+
+            <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Wishlist Books" checked />
+            <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">Tab content 2</div>
+
+            
+            </div>
         </>
     );
 };
