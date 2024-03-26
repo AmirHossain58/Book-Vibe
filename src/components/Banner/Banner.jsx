e972f31edf1d3b77ebd83175518a69285c2eab7c
AmirHossain58 <ahantar58@gmail.com>
2024-03-26 21:32:02 +0600

    book card section  added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,13 +3,16 @@ import { Typography } from '@material-tailwind/react';
 // import { Input } from 'postcss';
 
 import { Button } from "@material-tailwind/react";
+import { Link } from 'react-router-dom';
 const Banner = () => {
       return ( 
           <div className='h-[400px] mt-16 bg-[#1313130D] rounded-2xl gap-20 flex justify-center items-center'>
 
              <div className=''>
              <Typography variant="h2">Books to freshen <br /> up your bookshelf</Typography><br />
+             <Link to={'/listedBooks'}>
              <Button className='mt-6 ' color='green' variant="gradient">View The List</Button>
+             </Link>
              </div>
              <div className=' w-[350px]'><img className='bg-cover bg-no-repeat rounded-2xl h-[350px] w-[450px]' src="/public/stack-various-books-table.png" alt="" /></div>
           </div>
--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import { Button, Typography } from '@material-tailwind/react';
+import React, { useState } from 'react';
+import { GoChevronDown } from "react-icons/go";
 
 const ListedBooks = () => {
+    const[click ,setclick]=useState(false)
+
     return (
-        <div>
-            
+        <>
+        <div  className='h-[100px] flex justify-center items-center bg-[#1313130D] rounded-lg my-10'>
+        <Typography className='text-center ' variant="h1">Books</Typography>
         </div>
+        <div className='flex flex-col items-center justify-center  rounded-xl'>
+       <div> <Button color='green' className='flex gap-3' onClick={()=>setclick(!click)}>Sort By<GoChevronDown className='text-xl' /></Button></div>
+        <div className={click?'inline':'hidden'}>
+            <ul className={'bg-[#1313130D] p-2 rounded-2xl'}>
+            <li>Rating</li>
+            <li>Number of pages</li>
+            <li>Publisher year</li>
+        </ul></div>
+        </div>
+        </>
     );
 };
 
